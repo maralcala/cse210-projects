17dbbd76696034309c38a7ee77ed4af0079eff98
maralcala <mmarlene221@gmail.com>
2025-11-12 19:21:58 -0700

    Week 2 Activity Completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Projects Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Job job3 = new Job();
+        job3._company = "Adobe";
+        job3._jobTitle = "Head Software Engineer";
+        job3._startYear = 2023;
+        job3._endYear = 2025;
+       
+        Resume myResume = new Resume();
+        myResume._name = "Mar Alcalá";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job3);
+
+        myResume.Display();
     }
 }
